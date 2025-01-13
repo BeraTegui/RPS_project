336f281e5ebcc2bb1c1c4b92bf67a93fc1d4a2a3
BeraTegui <manubased@gmail.com>
2025-01-13 11:26:28 -0300

    feat new UI of game
    This new feature includes the new UI of the
    RPS game, including some DOM to make the buttons
    on the html work properly when pressed, with
    some event listeners and creating a div element
    to display the results after each round.

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Commented to work on branching TOP lesson
+
+/* 
+let playGame = () =>{
+    let i = 0;
+    while(i < 5){
+        playRound();
+        i++;
+    };
+    if(humanScore > computerScore){
+        console.log(` The game has finished...You're the winner! Congratulations Your score is: ${humanScore} and the computer's score is: ${computerScore}`)
+    } else if(humanScore === computerScore){
+        console.log(`It's a tie, no one wins! Your score is: ${humanScore} and the computer's score is: ${computerScore}`)
+    } else{
+        console.log(` The game has finished...Computer wins! Your score is: ${humanScore} and the computer's score is: ${computerScore}`)
+    }
+}
+ */
+
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -16,38 +39,46 @@ let getComputerChoice = () =>{
 }
 
 let getHumanChoice = () =>{
-    humanChoice = prompt("What is your choice??")
+    humanChoice = event.target.textContent;
     humanChoice = humanChoice.toLowerCase();
 }
-
 
 let playRound = () =>{
     getComputerChoice();
     getHumanChoice();
     if(humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock"){
-        console.log("You Won!!");
+        /* console.log("You Won!!"); */
         humanScore++;
-        console.log(`Your score is: ${humanScore} and the computer's score is: ${computerScore}`)
+        /* console.log(`Your score is: ${humanScore} and the computer's score is: ${computerScore}`) */
     } else if(computerChoice === humanChoice){
-        console.log("It's a tie! No one wins")
+        /* console.log("It's a tie! No one wins") */
     }else{
-        console.log("You lost!!")
+        /* console.log("You lost!!") */
         computerScore++;
-        console.log(`Your score is: ${humanScore} and the computer's score is: ${computerScore}`)
+        /* console.log(`Your score is: ${humanScore} and the computer's score is: ${computerScore}`) */
     }
 }
 
-let playGame = () =>{
-    let i = 0;
-    while(i < 5){
-        playRound();
-        i++;
-    };
-    if(humanScore > computerScore){
-        console.log(` The game has finished...You're the winner! Congratulations Your score is: ${humanScore} and the computer's score is: ${computerScore}`)
-    } else if(humanScore === computerScore){
-        console.log(`It's a tie, no one wins! Your score is: ${humanScore} and the computer's score is: ${computerScore}`)
-    } else{
-        console.log(` The game has finished...Computer wins! Your score is: ${humanScore} and the computer's score is: ${computerScore}`)
-    }
-}
+rock.addEventListener('click', () =>{
+    playRound();
+    const results = document.createElement('div');
+    results.textContent = "Your score: " + humanScore + " \nComputer's Score: " + computerScore;
+    document.querySelector('body').appendChild(results);
+
+});
+paper.addEventListener('click', () =>{
+    playRound();
+    const results = document.createElement('div');
+    results.textContent = "Your score: " + humanScore + " \nComputer's Score: " + computerScore;
+    document.querySelector('body').appendChild(results);
+});
+scissors.addEventListener('click', () =>{
+    playRound();
+    const results = document.createElement('div');
+    results.textContent = "Your score: " + humanScore + " \nComputer's Score: " + computerScore;
+    document.querySelector('body').appendChild(results);
+});
+
+
+
+
